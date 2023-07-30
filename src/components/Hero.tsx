06aecc18fb9a7a664e80d20b54cbecc89d4340c5
@yordi68 <yordanos.yirgu1@gmail.com>
2023-07-30 20:04:23 +0300

    finish the navbar and create a hero section

---
+++ src/components/Hero.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div>
+      <div className='container flex flex-col-reverse md:flex-row 
+      items-center px-6 mx-auto mt-10 space-y-0 md:space-y-0'>
+
+
+      </div>
+    </div>
+  )
+}
+
+export default Hero
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,13 +4,24 @@ import Logo from '../assets/logo.svg';
 
 const Navbar = () => {
   return (
-    <nav className='relative container mx-auto p-6 bg-red-500'> 
+    <nav className='relative container mx-auto p-6'> 
         <div className='flex flex-row items-center justify-between'>
             {/* logo */}
             <div className='pt-2'>
                 <img src={Logo} alt="Logo" />
-
             </div>
+            
+            <div className='hidden md:flex space-x-6'>
+              <a href="#" className='hover:text-darkGrayishBlue'>Pricing</a>
+              <a href="#" className='hover:text-darkGrayishBlue'>Product</a>
+              <a href="#" className='hover:text-darkGrayishBlue'>About Us</a>
+              <a href="#" className='hover:text-darkGrayishBlue'>Careers</a>
+              <a href="#" className='hover:text-darkGrayishBlue'>Community</a>
+            </div>
+
+            <a href="" className='hidden md:block p-3 px-6 pt-2 text-white bg-brightRed
+            rounded-full baseline hover:bg-brightRedLight'>Get Started</a>
+
 
         </div>
       
