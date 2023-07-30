7918a514590c93c3a4a440b25ac56248388189bb
@yordi68 <yordanos.yirgu1@gmail.com>
2023-07-30 20:36:17 +0300

    Finish hero section

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import Navbar from './components/Navbar';
 import './App.css'
-
+import Hero from './components/Hero';
 function App() {
 
   return (
     <>
       <Navbar />
+      <Hero />
     </>
   )
 }
