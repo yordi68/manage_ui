8597b136b461247e22a97d2e250306406339da40
@yordi68 <yordanos.yirgu1@gmail.com>
2023-08-01 04:44:00 +0300

    fix the features page

--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -20,6 +20,8 @@ const Features = () => {
             </div>
 
             <div className='flex flex-col space-y-8 md:w-1/2'>
+
+                
                 <div className='flex flex-col space-y-3 md:space-y-0 md:space-x-6 md:flex-row '>
                     <div className='rounded-l-full bg-brightRedSupLight md:bg-transparent'>
                         <div className='flex items-center space-x-2'>
@@ -35,6 +37,66 @@ const Features = () => {
                         <h3 className='hidden mb-4 text-lg font-bold md:block'>
                             Track your-company progress
                         </h3>
+
+                        <p className='text-darkGrayishBlue'>
+                            See how your day-to-day tasks fit into the wider
+                            vision. Go from 
+                            tracking progress at the milestone level all the way
+                            done to the smallest of details. Never lose sight of
+                            the bigger picture again.
+                        </p>
+                    </div>
+                </div>
+
+
+                               
+                <div className='flex flex-col space-y-3 md:space-y-0 md:space-x-6 md:flex-row '>
+                    <div className='rounded-l-full bg-brightRedSupLight md:bg-transparent'>
+                        <div className='flex items-center space-x-2'>
+                            <div className='px-4 py-2 text-white rounded-full md:py-1 bg-brightRed'>
+                                01
+                            </div>
+                            <h3 className='text-base font-bold md:mb-4 md:hidden'>
+                                Advanced built-in reports
+                            </h3>
+                        </div>
+                    </div>
+                    <div>
+                        <h3 className='hidden mb-4 text-lg font-bold md:block'>
+                            Advanced built-in reports
+                        </h3>
+
+                        <p className='text-darkGrayishBlue'>
+                            Set internal delivery estimates and track progress
+                            toward company goals. Our customizable dashboard helps you
+                            built out the reports you need to keep key stakeholders informed. 
+                        </p>
+                    </div>
+                </div>
+
+
+                               
+                <div className='flex flex-col space-y-3 md:space-y-0 md:space-x-6 md:flex-row '>
+                    <div className='rounded-l-full bg-brightRedSupLight md:bg-transparent'>
+                        <div className='flex items-center space-x-2'>
+                            <div className='px-4 py-2 text-white rounded-full md:py-1 bg-brightRed'>
+                                01
+                            </div>
+                            <h3 className='text-base font-bold md:mb-4 md:hidden'>
+                                Everything you need in one place.
+                            </h3>
+                        </div>
+                    </div>
+                    <div>
+                        <h3 className='hidden mb-4 text-lg font-bold md:block'>
+                            Everything you need in one place.
+                        </h3>
+
+                        <p className='text-darkGrayishBlue'>
+                            Stop jumping from one service to another to communicate,
+                            store files, track tasks and share documents. Manage offers
+                            an all-in-one team productivity solution.
+                        </p>
                     </div>
                 </div>
 
